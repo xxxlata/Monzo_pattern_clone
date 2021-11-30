a8fc6d81eef427d0f5e8e77bee354db9d937d611
bigtoe <xxxlata@naver.com>
2021-11-30 23:51:07 +0900

    clone

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const clickHandler = document.querySelector(".menu_last");
 const clickMenu = document.querySelector(".mobile_menu");
+const close = document.querySelector("#mobile_i");
 
 function open(){
     clickMenu.style.display = "block";
 }
+
+function closed(){
+    clickMenu.style.display = "none";
+}
   
 clickHandler.addEventListener("click",open);
+close.addEventListener("click",closed);
